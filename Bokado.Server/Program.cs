6c1d3c569714f7d289dd8c3c69214b3be68fd241
NureKolesnykOleksandr <oleksandr.kolesnyk@nure.ua>
2026-05-26 17:01:43 +0300

    Update Program.cs

--- Bokado.Server/Program.cs
+++ Bokado.Server/Program.cs
@@ -21,8 +21,8 @@ if (rawUrl != null)
 {
     var uri = new Uri(rawUrl);
     var userInfo = uri.UserInfo.Split(':');
-   connectionString = $"Host={uri.Host};Port={uri.Port};Database={uri.AbsolutePath.TrimStart('/')};Username={userInfo[0]};Password={userInfo[1]};SSL Mode=Disable";
-    Console.WriteLine($"Using connection: Host={uri.Host}");
+ connectionString = $"Host={uri.Host};Port={uri.Port};Database={uri.AbsolutePath.TrimStart('/')};Username={userInfo[0]};Password={userInfo[1]};SSL Mode=Disable";
+Console.WriteLine($"Full connection string: Host={uri.Host};Port={uri.Port};Database={uri.AbsolutePath.TrimStart('/')}");
 }
 else
 {
