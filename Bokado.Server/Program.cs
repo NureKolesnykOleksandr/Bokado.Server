b0d83e948e9d799e3385ceb718ad629987fd7df6
NureKolesnykOleksandr <oleksandr.kolesnyk@nure.ua>
2026-05-28 19:47:08 +0300

    Update Program.cs

--- Bokado.Server/Program.cs
+++ Bokado.Server/Program.cs
@@ -107,7 +107,7 @@ builder.Services.AddAuthentication(options =>
             var accessToken = context.Request.Query["access_token"];
             var path = context.HttpContext.Request.Path;
             if (!string.IsNullOrEmpty(accessToken) &&
-                path.StartsWithSegments("/hubs/notifications"))
+                (path.StartsWithSegments("/hubs/notifications") || path.StartsWithSegments("/hubs/chat"))
             {
                 context.Token = accessToken;
             }
