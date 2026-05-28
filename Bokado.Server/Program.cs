65a09cbc376c5d2fd7bbca9a2a31ce4c08c26d38
NureKolesnykOleksandr <oleksandr.kolesnyk@nure.ua>
2026-05-28 19:54:54 +0300

    Update Program.cs

--- Bokado.Server/Program.cs
+++ Bokado.Server/Program.cs
@@ -103,16 +103,17 @@ builder.Services.AddAuthentication(options =>
     options.Events = new JwtBearerEvents
     {
         OnMessageReceived = context =>
-        {
-            var accessToken = context.Request.Query["access_token"];
-            var path = context.HttpContext.Request.Path;
-            if (!string.IsNullOrEmpty(accessToken) &&
-                (path.StartsWithSegments("/hubs/notifications") || path.StartsWithSegments("/hubs/chat"))
-            {
-                context.Token = accessToken;
-            }
-            return Task.CompletedTask;
-        }
+{
+    var accessToken = context.Request.Query["access_token"];
+    var path = context.HttpContext.Request.Path;
+    if (!string.IsNullOrEmpty(accessToken) &&
+        (path.StartsWithSegments("/hubs/notifications") ||
+         path.StartsWithSegments("/hubs/chat")))
+    {
+        context.Token = accessToken;
+    }
+    return Task.CompletedTask;
+}
     };
 });
 
